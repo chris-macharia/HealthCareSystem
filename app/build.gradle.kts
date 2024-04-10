c6cfaa0c0b79f3cb4b5aef567302a8cd36d179bc
Chris Macharia <christophermaina003@gmail.com>
2024-04-10 16:49:40 +0300

    Design a login page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.healthcaresystem"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.healthcaresystem"
-        minSdk = 24
-        targetSdk = 33
+        minSdk = 21
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
