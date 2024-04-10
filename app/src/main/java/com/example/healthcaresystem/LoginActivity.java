6b29767fb8f21c0a50f4bbd29e7429f4f5209239
Chris Macharia <christophermaina003@gmail.com>
2024-04-10 18:13:40 +0300

    Change LoginActivity file to Java

---
+++ app/src/main/java/com/example/healthcaresystem/LoginActivity.java
@@ -0,0 +1,12 @@
+package com.example.healthcaresystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
