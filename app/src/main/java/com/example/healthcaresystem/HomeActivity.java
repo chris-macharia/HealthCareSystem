9e9b401dd3f2bf62d9784eb748a7117cdb578d7f
Chris Macharia <christophermaina003@gmail.com>
2024-04-15 15:41:03 +0300

    Enable log out on clicking the Log out button on home screen

--- app/src/main/java/com/example/healthcaresystem/HomeActivity.java
+++ app/src/main/java/com/example/healthcaresystem/HomeActivity.java
@@ -1,8 +1,16 @@
 package com.example.healthcaresystem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.security.PublicKey;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +18,28 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //Get the User name from Login Page then display as welcome message
+        SharedPreferences sharedPreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
+        String username = sharedPreferences.getString("username", "").toString();
+        Toast.makeText(getApplicationContext(), "Welcome " + username, Toast.LENGTH_SHORT).show();
+
+        //Log Out Button
+        CardView logoutBtn = findViewById(R.id.cardLogout);
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Clear the stored Username and exit
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.clear();
+                editor.apply();
+                startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+            }
+        });
+
+
+
+
+
     }
 }
